43e932e995b9c9879ea7b4ade05e1e23562c89ea
soyoung <jih32002@naver.com>
2023-08-08 15:56:46 +0900

    Initial Commit

---
+++ purplepillow_last/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  purplepillow_last
+//
+//  Created by kim yeon kyung on 2023/08/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ purplepillow_lastUITests/purplepillow_lastUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  purplepillow_lastUITestsLaunchTests.swift
+//  purplepillow_lastUITests
+//
+//  Created by kim yeon kyung on 2023/08/08.
+//
+
+import XCTest
+
+final class purplepillow_lastUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
